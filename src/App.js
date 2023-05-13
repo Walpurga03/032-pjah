fb6f97b8c97f1f8d837c03bcd4ddad31e4f9a400
Alex <mokiskurve007@gmail.com>
2023-05-13 17:27:30 +0200

    test init

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hier endsteht die neue Website von pjah
+          Hier entsteht die neue Website von pjah
         </a>
       </header>
     </div>
