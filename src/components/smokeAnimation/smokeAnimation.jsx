7fd3a5c28e056ffcdebed45dcc955ec8bb17146e
Alex <mokiskurve007@gmail.com>
2023-06-26 11:26:08 +0200

    fixSmokeAnimatiom

--- src/components/smokeAnimation/smokeAnimation.jsx
+++ src/components/smokeAnimation/smokeAnimation.jsx
@@ -1,53 +1,96 @@
 import "./smokeAnimation.scss";
-import { Fragment, useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Video from "../smokeAnimation/smoke.mp4";
 
 const SmokeAnimation = () => {
   const [videoLoaded, setVideoLoaded] = useState(false);
+
   useEffect(() => {
-    const videoElement = document.getElementById("smokeVideo");
-
-    const handleVideoLoad = () => {
-      setVideoLoaded(true);
-    };
-    videoElement.addEventListener("canplaythrough", handleVideoLoad);
-
-    return () => {
-      videoElement.removeEventListener("canplaythrough", handleVideoLoad);
-    };
+    setVideoLoaded(true);
   }, []);
 
   return (
-    <Fragment>
-      <div
-        className="smokeAnimation-body"
-        style={{ background: videoLoaded ? "transparent" : "black" }}
-      >
-        {!videoLoaded && <div>Loading...</div>}
-        <video
-          id="smokeVideo"
-          src={Video}
-          autoPlay
-          muted
-          preload="auto"
-          style={{ display: videoLoaded ? "block" : "none" }}
-          onLoadedData={() => setVideoLoaded(true)}
-        ></video>
-        {videoLoaded && (
-          <h1 className="smokeAnimation-h1">
-            <span>Ü</span>
-            <span>B</span>
-            <span>E</span>
-            <span>R</span>
-            <span>&nbsp;</span>
-            <span>U</span>
-            <span>N</span>
-            <span>S</span>
-          </h1>
-        )}
-      </div>
-    </Fragment>
+    <div
+      className="smokeAnimation-body"
+      style={{ background: videoLoaded ? "transparent" : "black" }}
+    >
+      {!videoLoaded && <div>Loading...</div>}
+      <video
+        id="smokeVideo"
+        src={Video}
+        autoPlay
+        muted
+        preload="auto"
+        style={{ display: videoLoaded ? "block" : "none" }}
+        onLoadedData={() => setVideoLoaded(true)}
+      ></video>
+      {videoLoaded && (
+        <h1 className="smokeAnimation-h1">
+          <span>Ü</span>
+          <span>B</span>
+          <span>E</span>
+          <span>R</span>
+          <span>&nbsp;</span>
+          <span>U</span>
+          <span>N</span>
+          <span>S</span>
+        </h1>
+      )}
+    </div>
   );
 };
 
 export default SmokeAnimation;
+
+// import { Fragment, useState, useEffect } from "react";
+// import Video from "../smokeAnimation/smoke.mp4";
+
+// const SmokeAnimation = () => {
+//   const [videoLoaded, setVideoLoaded] = useState(false);
+//   useEffect(() => {
+//     const videoElement = document.getElementById("smokeVideo");
+
+//     const handleVideoLoad = () => {
+//       setVideoLoaded(true);
+//     };
+//     videoElement.addEventListener("canplaythrough", handleVideoLoad);
+
+//     return () => {
+//       videoElement.removeEventListener("canplaythrough", handleVideoLoad);
+//     };
+//   }, []);
+
+//   return (
+//     <Fragment>
+//       <div
+//         className="smokeAnimation-body"
+//         style={{ background: videoLoaded ? "transparent" : "black" }}
+//       >
+//         {!videoLoaded && <div>Loading...</div>}
+//         <video
+//           id="smokeVideo"
+//           src={Video}
+//           autoPlay
+//           muted
+//           preload="auto"
+//           style={{ display: videoLoaded ? "block" : "none" }}
+//           onLoadedData={() => setVideoLoaded(true)}
+//         ></video>
+//         {videoLoaded && (
+//           <h1 className="smokeAnimation-h1">
+//             <span>Ü</span>
+//             <span>B</span>
+//             <span>E</span>
+//             <span>R</span>
+//             <span>&nbsp;</span>
+//             <span>U</span>
+//             <span>N</span>
+//             <span>S</span>
+//           </h1>
+//         )}
+//       </div>
+//     </Fragment>
+//   );
+// };
+
+// export default SmokeAnimation;
