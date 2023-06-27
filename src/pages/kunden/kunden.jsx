fd92cb1b6f7cf480c0bdce3798a104ea12c8a181
Alex <mokiskurve007@gmail.com>
2023-06-27 11:23:07 +0200

    testResponsiveKunde

--- src/pages/kunden/kunden.jsx
+++ src/pages/kunden/kunden.jsx
@@ -1,9 +1,49 @@
-import React, { Fragment } from "react";
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 
-import "./kunden.scss";
+function Kunde() {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-const Kunden = () => {
-  return <Fragment>Unsere Kunden</Fragment>;
-};
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
 
-export default Kunden;
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const breakpoints = [
+    { width: 1200, fontSize: "4rem", color: "red" },
+    { width: 992, fontSize: "3rem", color: "blue" },
+    { width: 768, fontSize: "2rem", color: "green" },
+  ];
+
+  const getStyles = (width) => {
+    const { fontSize, color } = breakpoints.find(
+      (breakpoint) => width >= breakpoint.width
+    ) || { fontSize: "1rem", color: "black" };
+
+    return { fontSize, color };
+  };
+
+  const { fontSize, color } = getStyles(windowWidth);
+
+  return (
+    <Container fontSize={fontSize} color={color}>
+      <p className="centered">Bildschirmbreite: {windowWidth}</p>
+    </Container>
+  );
+}
+
+export default Kunde;
+
+const Container = styled.div`
+  margin-top: 20rem;
+  font-size: ${(props) => props.fontSize};
+  color: ${(props) => props.color};
+
+  .centered {
+    text-align: center;
+  }
+`;
